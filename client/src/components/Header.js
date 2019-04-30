e0566fc15616c31cff25275bebee8e8bc73afd8e
Minh Trí <MinhTri@TmTzMinhTri.net.fpt>
2019-05-01 04:52:43 +0700

    create Header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,14 @@ import React from 'react'
 export default class Header extends React.Component {
     render() {
         return(
-            <div></div>
+            <nav>
+                <div className='nav-wrapper'>
+                    <a className ='left brand-logo'>Tiny Shop</a>
+                    <ul className='right'>
+                        <li><a>Login with google</a></li>
+                    </ul>
+                </div>
+            </nav>
         )
     }
 }
