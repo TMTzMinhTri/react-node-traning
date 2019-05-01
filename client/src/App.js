44852555231d8924d0d368ee4b4eb519ae5c501c
Minh Trí <MinhTri@TmTzMinhTri.net.fpt>
2019-05-02 02:56:13 +0700

    redirect a user on auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import './App.css';
-
 import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from './actions'
 
 import Header from './components/Header'
 const Dashboard = () => <h2>Dashboard</h2>
 const Landing = () => <h2>Landing</h2>
 
 
-function App() {
-  return (
-    <div className='container'>
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/surveys' component={Dashboard} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser()
+  }
+  render(){
+    return (
+      <div className='container'>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/surveys' component={Dashboard} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(null,actions)(App);
