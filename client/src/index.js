4a7b8c4a4647ad349765a155a49252235c052f68
Minh Trí <MinhTri@TmTzMinhTri.net.fpt>
2019-05-04 03:00:39 +0700

    add setup proxy

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import 'materialize-css/dist/css/materialize.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reduxThunk from "redux-thunk";
