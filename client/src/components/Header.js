fbd7eff324f492e388ddd8387817becdc94eb3ed
Minh Trí <MinhTri@TmTzMinhTri.net.fpt>
2019-05-06 00:13:23 +0700

    set env client

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,6 @@ import {Link} from 'react-router-dom'
 
 export default connect(mapStateToProps)(class Header extends React.Component {
     renderContext = () => {
-        console.log(this.props.auth)
         switch (this.props.auth) {
             case null:
                 return
--- config/dev.js
+++ config/dev.js
@@ -2,5 +2,7 @@ module.exports = {
     googleClientID: "394911232807-pg2m5fpprqbg1c7n2i4gu2d92atfus6e.apps.googleusercontent.com",
     googleClientSecret: "UYoqaDYtIK47uM-r8jsjvWYJ",
     mongoURI : "mongodb://localhost:27017/learning_fullstack",
-    cookieKey: "afjhasdfuashdfusdhfslfsd"  
+    cookieKey: "afjhasdfuashdfusdhfslfsd"  ,
+    stripePublicableKey : "pk_test_OTQBbjvfr69jn5oIOTwCPZdw00TW01iSw9",
+    stripeSecretKey: "sk_test_bIVsURD3r7Pm02elnpxatIel00U7KQybMe"
 }
