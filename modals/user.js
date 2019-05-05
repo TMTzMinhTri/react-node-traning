97758ac62ba64a9443260fbf408e44ab15beb024
Minh Trí <MinhTri@TmTzMinhTri.net.fpt>
2019-05-06 02:54:45 +0700

    api stripe

--- modals/user.js
+++ modals/user.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema ({
+const userSchema = new Schema({
     googleID: String,
+    credits: {
+        type: Number,
+        default: 0
+    }
 })
 
-mongoose.model('user',userSchema)
+mongoose.model('user', userSchema)
